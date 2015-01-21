1d5238dce01a25f836f650afd0d1a479f08d5c1a
meloniq <meloniq@gmail.com>
2015-01-21 17:25:57 +0100

    add collapse/expand code for gist search - while searching gists it displays full listings which is annoying, and with this hack it shows only 10 lines, full view only after clicking button, also restyles gist link overlay so its possible to copy code without visiting single gist page, fixes issue with displaying code which sometimes get pushed down

---
+++ github.com/Gist-Search/gist-search.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        Github Gist - Collapse-Expand Code in Search
+// @namespace   https://github.com/meloniq/user-styles-scripts/tree/master/github.com/Gist-Search
+// @description Github Gist - Collapse-Expand Code in Search
+// @include     *gist.github.com/search*
+// @run-at      document-body
+// @author      meloniq
+// @homepage    http://blog.meloniq.net
+// @license     GNU General Public License v2 or later
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+
+
+(function($) {
+
+	function gist_add_expand_links() {
+		$('.file-box').not( ':has(> a.gist-expand)' ).each(function() {
+			$(this).prepend('<a href="#" class="gist-expand"><span>Expand/Collapse</span></a>');
+		});
+	}
+	gist_add_expand_links();
+
+	jQuery(document).ajaxComplete( function() { 
+		gist_add_expand_links();
+	});
+
+	$(document).on('click', '.gist-expand', function() {
+	   $(this).closest('.gist-item').toggleClass('expand');
+	   return false;
+	});
+}(unsafeWindow.jQuery));
